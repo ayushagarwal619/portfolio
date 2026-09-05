3175effd1138ec7ac14a1f132a5e7b57ff0d61bc
ayushagarwal619 <ayushtechnoworld@gmail.com>
2026-09-05 13:12:27 +0530

    chore: remove unused legacy assets

--- data/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Central Data Exports
- */
-
-export * from "./bioData";
-export * from "./projectsData";
-export * from "./certificatesData";
-export * from "./experienceData";
-export * from "./techStackData";
